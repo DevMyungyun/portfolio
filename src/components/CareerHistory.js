ff3cf08c2e69bae5a40993a02789a17aa021ee4c
DevMyungyun <myungyuncho82@gmail.com>
2022-11-15 01:00:54 -0600

    add career page

--- src/components/CareerHistory.js
+++ src/components/CareerHistory.js
@@ -1,25 +1,35 @@
 import React from 'react';
-import {Tab, Tabs, TabList, TabPanel} from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import './CareerHistory.css';
 
-function Career(props) {
+function CareerHistory(props) {
+    console.log(props);
+    const career = props.CareerHistory.career;
+    const tab = career.tab;
+    const tabPanel = career.tabPanel;
     return (
-        <div>
-            <Tabs>
-                <TabList>
-                    <Tab>Title 1</Tab>
-                    <Tab>Title 2</Tab>
-                </TabList>
-
-                <TabPanel>
-                    <h2>Any content 1</h2>
-                </TabPanel>
-                <TabPanel>
-                    <h2>Any content 2</h2>
-                </TabPanel>
-            </Tabs>
+        <div className="App">
+      <Tabs>
+        <TabList>
+            {
+                tab.map(el => {
+                return <Tab><p>{el}</p></Tab>
+})     
+            }
+        </TabList>
+{
+    tabPanel.map(el => {
+        return <TabPanel>
+        <div className="panel-content">
+          {el.description}<br></br>
         </div>
+      </TabPanel>
+    })
+}
+       
+      </Tabs>
+      </div>
     );
 }
 
-export default Career;
+export default CareerHistory;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {Responsive, WidthProvider} from "react-grid-layout";
+import '../../node_modules/react-grid-layout/css/styles.css';
+import '../../node_modules/react-resizable/css/styles.css';
 import styled from "styled-components";
 
 const ResponsiveGridLayout = WidthProvider(Responsive);
@@ -19,28 +21,32 @@ const layout = [
         y: 0,
         w: 2.5,
         h: 1.5,
-        minW: 1, maxW: 3
+        minW: 1,
+        maxW: 3
     }, {
         i: "picture",
         x: 3,
         y: 0,
         w: 2.5,
         h: 1.5,
-        minW: 1, maxW: 3
+        minW: 1,
+        maxW: 3
     }, {
         i: "career",
         x: 0,
         y: 1,
         w: 2,
         h: 1,
-        minW: 1, maxW: 2
+        minW: 1,
+        maxW: 2
     }, {
         i: "portfolio",
         x: 2,
         y: 1,
         w: 2,
         h: 1,
-        minW: 1, maxW: 2
+        minW: 1,
+        maxW: 2
     }, {
         i: "contact",
         x: 4,
@@ -52,44 +58,61 @@ const layout = [
 
 class Home extends Component {
     render() {
+        console.log(this.props);
+        const contents = this.props.home;
         return (
-            <div>
+            <div style={{"position": "relative"}}>
                 <ResponsiveGridLayout
-                layouts={{
-                lg: layout
-            }}
-                breakpoints={{
-                lg: 1200,
-                md: 996,
-                sm: 768,
-                xs: 480,
-                xxs: 0
-            }}
-                cols={{
-                lg: 5,
-                md: 4,
-                sm: 3,
-                xs: 2,
-                xxs: 1
-            }}
-                rowHeight={300}
-                width={1000}>
-                <GridItemWrapper key="about">
-                    <GridItemContent>about</GridItemContent>
-                </GridItemWrapper>
-                <GridItemWrapper key="picture">
-                    <GridItemContent>picture</GridItemContent>
-                </GridItemWrapper>
-                <GridItemWrapper key="career">
-                    <GridItemContent>career</GridItemContent>
-                </GridItemWrapper>
-                <GridItemWrapper key="portfolio">
-                    <GridItemContent>portfolio</GridItemContent>
-                </GridItemWrapper>
-                <GridItemWrapper key="contact">
-                    <GridItemContent>contact</GridItemContent>
-                </GridItemWrapper>
-            </ResponsiveGridLayout>
+                    layouts={{
+                    lg: layout
+                }}
+                    breakpoints={{
+                    lg: 1200,
+                    md: 996,
+                    sm: 768,
+                    xs: 480,
+                    xxs: 0
+                }}
+                    cols={{
+                    lg: 5,
+                    md: 4,
+                    sm: 3,
+                    xs: 2,
+                    xxs: 1
+                }}
+                    rowHeight={300}
+                    width={1000}>
+                    <GridItemWrapper key="about">
+                        <GridItemContent>
+                            {contents
+                                .intro
+                                .map(el => <div>
+                                    <span>{el}</span><br/></div>)
+}
+                            {contents
+                                .positions
+                                .map(el => <span>{el + " / "}</span>)
+}
+                            {contents
+                                .description
+                                .map(el => <div>
+                                    <span>{el}</span><br/></div>)
+}
+                        </GridItemContent>
+                    </GridItemWrapper>
+                    <GridItemWrapper key="picture">
+                        <GridItemContent>picture</GridItemContent>
+                    </GridItemWrapper>
+                    <GridItemWrapper key="career">
+                        <GridItemContent>career</GridItemContent>
+                    </GridItemWrapper>
+                    <GridItemWrapper key="portfolio">
+                        <GridItemContent>portfolio</GridItemContent>
+                    </GridItemWrapper>
+                    <GridItemWrapper key="contact">
+                        <GridItemContent>contact</GridItemContent>
+                    </GridItemWrapper>
+                </ResponsiveGridLayout>
             </div>
         );
     }
