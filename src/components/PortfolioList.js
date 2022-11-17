610d85760af9f4711dbb31d7684f02a88c66b394
DevMyungyun <myungyuncho82@gmail.com>
2022-11-17 00:57:56 -0600

    add career page draft

---
+++ src/components/PortfolioList.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+
+function PortfolioList(props) {
+
+    useEffect() {
+        
+    }
+    return (
+        <div>
+            <tabl>
+                <tr>
+                    project name
+                </tr>
+                <tr>
+                <td>
+                        <video></video>
+                    </td>
+                    <td>
+                        description
+                        
+                    </td>
+                </tr>
+            </tabl>
+        </div>
+    );
+}
+
+export default PortfolioList;
